62e8226d875e22cc8636a04dcf687e895c1fd944
flavien <flavien.cotreau@gmail.com>
2023-06-06 06:29:19 +0200

    creating item table

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'item/index'
+  get 'item/new'
+  get 'item/show'
+  get 'item/create'
+  get 'item/edit'
+  get 'item/update'
+  get 'item/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
