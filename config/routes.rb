6e39b0131ad166636e64e95daa2fef9cf6fd14eb
flavien <flavien.cotreau@gmail.com>
2023-06-06 08:16:20 +0200

    updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,11 @@
 Rails.application.routes.draw do
-  get 'cart/index'
-  get 'cart/new'
-  get 'cart/show'
-  get 'cart/create'
-  get 'cart/edit'
-  get 'cart/update'
-  get 'cart/destroy'
-  get 'user/index'
-  get 'user/new'
-  get 'user/show'
-  get 'user/create'
-  get 'user/edit'
-  get 'user/update'
-  get 'user/destroy'
+  root 'item#index'
+  resources :item
+  resources :user
+  resources :cart
+
   devise_for :users
-  get 'item/index'
-  get 'item/new'
-  get 'item/show'
-  get 'item/create'
-  get 'item/edit'
-  get 'item/update'
-  get 'item/destroy'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
